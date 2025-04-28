3ca48a12a651544ffccdbf98a5063cf18315c65f
berkant arslan <berkant6169@gmail.com>
2025-04-29 02:47:05 +0300

    Day 1: Proje klasörü ve temel açıklama oluşturuldu

---
+++ quiz.py
@@ -0,0 +1,11 @@
+# quiz.py
+"""
+Python Quiz Uygulaması
+
+Bu uygulamada kullanıcıya çeşitli sorular sorulacak ve doğru cevaplarına göre puan verilecektir.
+Her soru için kullanıcıdan cevap alınacak ve doğru/yanlış kontrolü yapılacaktır.
+
+Proje Başlangıcı: Gün 1
+"""
+
+# Henüz kod yazılmadı. İlerleyen günlerde quiz fonksiyonları eklenecek.
