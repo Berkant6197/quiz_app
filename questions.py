f19874c92aaf311db50193693218026ef88d04b1
Berkant6197 <berkant6169@gmail.com>
2025-05-08 18:24:26 +0300

    10 yeni soru eklendi

--- questions.py
+++ questions.py
@@ -9,4 +9,54 @@ questions = [
         "options": ["A. Bill Gates", "B. Guido van Rossum", "C. Steve Jobs", "D. Linus Torvalds"],
         "answer": "B"
     }
+    {
+        "question": "Python'da bir değişken tanımlamak için hangi sembol kullanılır?",
+        "options": ["A) =", "B) :", "C) ==", "D) ->"],
+        "answer": "A"
+    },
+    {
+        "question": "Hangisi Python'da liste oluşturmak için kullanılır?",
+        "options": ["A) {}", "B) []", "C) ()", "D) <>"],
+        "answer": "B"
+    },
+    {
+        "question": "Python dilini geliştiren kişi kimdir?",
+        "options": ["A) Brendan Eich", "B) James Gosling", "C) Guido van Rossum", "D) Dennis Ritchie"],
+        "answer": "C"
+    },
+    {
+        "question": "Python’da bir fonksiyon nasıl tanımlanır?",
+        "options": ["A) func()", "B) define()", "C) function()", "D) def"],
+        "answer": "D"
+    },
+    {
+        "question": "Hangisi bir Python döngü yapısı değildir?",
+        "options": ["A) for", "B) while", "C) do-while", "D) None"],
+        "answer": "C"
+    },
+    {
+        "question": "Python’da ‘set’ veri tipi neyi temsil eder?",
+        "options": ["A) Sıralı veri", "B) Anahtar-değer çifti", "C) Eşsiz elemanlar koleksiyonu", "D) Tek bir karakter dizisi"],
+        "answer": "C"
+    },
+    {
+        "question": "Python’da ‘None’ ifadesi ne anlama gelir?",
+        "options": ["A) Sıfır", "B) Boş liste", "C) Tanımsız değer", "D) Hata"],
+        "answer": "C"
+    },
+    {
+        "question": "Hangisi Python’da hata (exception) türlerinden biridir?",
+        "options": ["A) NullError", "B) IndexError", "C) SegFault", "D) LoopError"],
+        "answer": "B"
+    },
+    {
+        "question": "Python’da yorum satırı hangi sembolle başlar?",
+        "options": ["A) //", "B) <!--", "C) #", "D) ;;"],
+        "answer": "C"
+    },
+    {
+        "question": "Python'da ‘lambda’ ne işe yarar?",
+        "options": ["A) Döngü oluşturur", "B) Koşul kontrolü yapar", "C) Anonim fonksiyon tanımlar", "D) Modül import eder"],
+        "answer": "C"
+    }
 ]
